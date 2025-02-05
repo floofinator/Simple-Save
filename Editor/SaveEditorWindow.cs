57f423fc8a206bce456ce64372de2ee2aaf188b2
floofinator <lukenthomson9@gmail.com>
2025-02-05 15:55:30 -0500

    Save editor wip

--- Editor/SaveEditorWindow.cs
+++ Editor/SaveEditorWindow.cs
@@ -8,24 +8,21 @@ namespace Floofinator.SimpleSave.Editor
 {
     public class SaveEditorWindow : EditorWindow
     {
-        JsonFiler filer;
-
         [MenuItem("Window/Save Editor")]
         static void Init()
         {
             GetWindow<SaveEditorWindow>("Save Editor");
         }
-        private void OnEnable()
-        {
-            filer = new();
-        }
         void CreateGUI()
         {
-            Button clearButton = new();
-            clearButton.Add(new Label("Clear Save"));
-            clearButton.clicked += () => SceneFiler.ClearScene(filer);
+            Label label = new ("WIP");
+            // Button clearButton = new();
+            // clearButton.Add(new Label("Clear Save"));
+            // clearButton.clicked += () => SceneFiler.ClearScene(filer);
 
-            rootVisualElement.Add(clearButton);
+            // rootVisualElement.Add(clearButton);
+
+            rootVisualElement.Add(label);
         }
     }
 }
